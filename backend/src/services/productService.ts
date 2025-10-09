2f8c1f7646a0cab021fe52c58f56929d54d1c0ed
Nada ali <elghoulnada17@gmail.com>
2025-10-10 00:13:49 +0300

    added items to Home

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -10,6 +10,9 @@ export const seedInitialProducts = async () => {
        {title: "Dell Laptop" , image: "https://th.bing.com/th/id/OIP.6f1SuqpxXrIJWcRPDK6UBwHaFj?w=231&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" , price: 15000 , stock: 10},
        {title: "Asus Laptop" , image: "https://th.bing.com/th/id/OIP.21ZjavQ-dwJSCHhEYC-RrAHaHa?o=7&cb=12rm=3&rs=1&pid=ImgDetMain&o=7&rm=3" , price: 25000 , stock: 20},
        {title: "HP Laptop" , image: "https://th.bing.com/th/id/R.f1d38d63cdaeb315d0cd91c9fce8307d?rik=77yW0%2fGz1ADu2w&pid=ImgRaw&r=0" , price: 40000 , stock: 8},
+       {title: "Lenovo Laptop" , image: "https://m.media-amazon.com/images/I/51ZpF7hDnvL._SL1040_.jpg" , price: 30000 , stock: 5},
+       {title: "Sony Laptop" , image: "https://tse1.mm.bing.net/th/id/OIP.jRuNwaGjzAnlaMdCKEOWywAAAA?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3" , price: 20000 , stock: 15},
+       {title: "Apple Laptop" , image: "https://www.laptop-doctor.com/wp-content/uploads/2019/10/Apple-13.3_-MacBook-Air-Mid-2017-Newest-Model.jpg" , price: 45000 , stock: 8},
     ];
 
     const existingProducts = await getAllProducts();
